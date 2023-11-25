cbb7eeeea341e63648843121d46a3d87d7d1953c
Kevin Ren <kevinrenfc@gmail.com>
2023-11-24 20:44:29 -0800

    Removed Stryker enable/disable in SectionsInstructorTable.js, also removed disableGroupBy vars

--- frontend/src/main/components/Sections/SectionsInstructorTable.js
+++ frontend/src/main/components/Sections/SectionsInstructorTable.js
@@ -11,26 +11,21 @@ import {
 } from "main/utils/sectionUtils.js";
 
 export default function SectionsInstructorTable({ sections }) {
-  // Stryker enable all
-  // Stryker disable BooleanLiteral
   const columns = [
     {
       Header: "Course ID",
       accessor: "courseInfo.courseId",
 
-      disableGroupBy: true,
       Cell: ({ cell: { value } }) => value.substring(0, value.length - 2),
     },
     {
       Header: "Quarter",
       accessor: (row) => yyyyqToQyy(row.courseInfo.quarter),
-      disableGroupBy: true,
       id: "quarter",
     },
     {
       Header: "Title",
       accessor: "courseInfo.title",
-      disableGroupBy: true,
     },
     {
       // Stryker disable next-line StringLiteral: this column is hidden, very hard to test
@@ -44,37 +39,31 @@ export default function SectionsInstructorTable({ sections }) {
       Header: "Enrolled",
       accessor: (row) =>
         convertToFraction(row.section.enrolledTotal, row.section.maxEnroll),
-      disableGroupBy: true,
       id: "enrolled",
     },
     {
       Header: "Location",
       accessor: (row) => formatLocation(row.section.timeLocations),
-      disableGroupBy: true,
       id: "location",
     },
     {
       Header: "Days",
       accessor: (row) => formatDays(row.section.timeLocations),
-      disableGroupBy: true,
       id: "days",
     },
     {
       Header: "Time",
       accessor: (row) => formatTime(row.section.timeLocations),
-      disableGroupBy: true,
       id: "time",
     },
     {
       Header: "Instructor",
       accessor: (row) => formatInstructors(row.section.instructors),
-      disableGroupBy: true,
       id: "instructor",
     },
     {
       Header: "Enroll Code",
       accessor: "section.enrollCode",
-      disableGroupBy: true,
     },
   ];
 
