45312992089346dcabfa4cbf1ac0e59147ae004b
Kevin Ren <kevinrenfc@gmail.com>
2023-11-27 16:52:29 -0800

    forgot to pass format checking

--- frontend/src/stories/components/SectionsInstructorTableBase.stories.js
+++ frontend/src/stories/components/SectionsInstructorTableBase.stories.js
@@ -25,52 +25,52 @@ const Template = (args) => {
 };
 
 const columns = [
-    {
-      Header: "Course ID",
-      accessor: "courseInfo.courseId",
+  {
+    Header: "Course ID",
+    accessor: "courseInfo.courseId",
 
-      Cell: ({ cell: { value } }) => value.substring(0, value.length - 2),
-    },
-    {
-      Header: "Quarter",
-      accessor: (row) => yyyyqToQyy(row.courseInfo.quarter),
-      id: "quarter",
-    },
-    {
-      Header: "Title",
-      accessor: "courseInfo.title",
-    },
-    {
-      Header: "Enrolled",
-      accessor: (row) =>
-        convertToFraction(row.section.enrolledTotal, row.section.maxEnroll),
-      id: "enrolled",
-    },
-    {
-      Header: "Location",
-      accessor: (row) => formatLocation(row.section.timeLocations),
-      id: "location",
-    },
-    {
-      Header: "Days",
-      accessor: (row) => formatDays(row.section.timeLocations),
-      id: "days",
-    },
-    {
-      Header: "Time",
-      accessor: (row) => formatTime(row.section.timeLocations),
-      id: "time",
-    },
-    {
-      Header: "Instructor",
-      accessor: (row) => formatInstructors(row.section.instructors),
-      id: "instructor",
-    },
-    {
-      Header: "Enroll Code",
-      accessor: "section.enrollCode",
-    },
-  ];
+    Cell: ({ cell: { value } }) => value.substring(0, value.length - 2),
+  },
+  {
+    Header: "Quarter",
+    accessor: (row) => yyyyqToQyy(row.courseInfo.quarter),
+    id: "quarter",
+  },
+  {
+    Header: "Title",
+    accessor: "courseInfo.title",
+  },
+  {
+    Header: "Enrolled",
+    accessor: (row) =>
+      convertToFraction(row.section.enrolledTotal, row.section.maxEnroll),
+    id: "enrolled",
+  },
+  {
+    Header: "Location",
+    accessor: (row) => formatLocation(row.section.timeLocations),
+    id: "location",
+  },
+  {
+    Header: "Days",
+    accessor: (row) => formatDays(row.section.timeLocations),
+    id: "days",
+  },
+  {
+    Header: "Time",
+    accessor: (row) => formatTime(row.section.timeLocations),
+    id: "time",
+  },
+  {
+    Header: "Instructor",
+    accessor: (row) => formatInstructors(row.section.instructors),
+    id: "instructor",
+  },
+  {
+    Header: "Enroll Code",
+    accessor: "section.enrollCode",
+  },
+];
 
 const testid = "SectionsInstructorTable";
 
