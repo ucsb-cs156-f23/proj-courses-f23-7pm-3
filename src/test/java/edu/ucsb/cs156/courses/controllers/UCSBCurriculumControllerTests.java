693600f2f788ede3d2dfde9573b708bea59a9fe2
mucucsb <mu52@umail.ucsb.edu>
2023-11-28 14:48:25 -0800

    Update UCSBCurriculumControllerTests.java

--- src/test/java/edu/ucsb/cs156/courses/controllers/UCSBCurriculumControllerTests.java
+++ src/test/java/edu/ucsb/cs156/courses/controllers/UCSBCurriculumControllerTests.java
@@ -25,7 +25,7 @@ import org.springframework.test.web.servlet.MvcResult;
 @Import(SecurityConfig.class)
 @AutoConfigureDataJpa
 public class UCSBCurriculumControllerTests {
-  
+
   private ObjectMapper mapper = new ObjectMapper();
 
   @MockBean UserRepository userRepository;
